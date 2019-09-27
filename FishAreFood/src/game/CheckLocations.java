1b828e0ac04f2bea5ced1b412f48c6b76ea508c1
Jack Paul <Jack Paul@MAIN-Computer>
2019-09-27 12:11:38 -0600

    Made game package to store game methods.

---
+++ FishAreFood/src/game/CheckLocations.java
@@ -0,0 +1,115 @@
+package game;
+
+public class CheckLocations
+{
+
+	public static int checkSharkLocationScore(int[][] itemLocation, int[][] sharkLocation, int score)
+	{
+		
+		//variables
+		
+		int itemCols = 24;
+		int sharkCols = 8;
+		
+		//checkSharkLocationVsItemLocation
+		
+		for (int ic = 0; ic < itemCols; ic++)
+		{
+
+			for (int sc = 0; sc < sharkCols; sc++)
+			{
+
+				if ((sharkLocation[0][sc] == itemLocation[0][ic] && sharkLocation[1][sc] == itemLocation[1][ic]) && itemLocation[2][ic] == 1)
+				{
+
+					itemLocation[2][ic] = 0;
+					score = score + 1;
+					
+				}
+				
+				if ((sharkLocation[0][sc] == itemLocation[0][ic] && sharkLocation[1][sc] == itemLocation[1][ic]) && itemLocation[2][ic] == 2)
+				{
+
+					itemLocation[2][ic] = 0;
+					score = score - 1;
+					
+				}
+
+			}				
+		
+		}
+		
+		return(score);
+		
+	}
+	
+	public static boolean checkSharkLocationLoose(String[][] gameScreen, int[][] itemLocation, int[][] sharkLocation, String mp, int score, boolean loose)
+	{
+		
+		//variables
+		
+		int rows = 11;
+		int cols = 24;
+		int itemCols = 24;
+		int sharkCols = 8;
+				
+		//checkSharkLocationVsItemLocation
+		
+		for (int ic = 0; ic < itemCols; ic++)
+		{
+
+			for (int sc = 0; sc < sharkCols; sc++)
+			{
+
+				gameScreen [sharkLocation[0][sc]][sharkLocation[1][sc]] = mp;
+				
+				if ((sharkLocation[0][sc] == itemLocation[0][ic] && sharkLocation[1][sc] == itemLocation[1][ic]) && itemLocation[2][ic] == 3)
+				{
+					
+					itemLocation[2][ic] = 0;
+
+					System.out.print("BOOM! You hit a mine. Game Over."); //"\n" if key scanners r added 
+					
+					System.out.print("\nFishRFood                            Score = " + score);
+					
+					for (int r = 0; r < rows; r++)
+					{
+						
+						if (r == 0)
+						{
+							
+							System.out.print("\n");
+							
+						}
+						
+						for (int c = 0; c < cols; c++)
+						{
+						
+							System.out.print(gameScreen [r][c]);
+							
+						}
+						
+						if (r < rows - 1)
+						{
+							
+							System.out.print("\n");
+						
+						}
+						
+					}
+					
+					System.out.print("\nM M M  BOOM! You hit a mine!? GAME OVER.  M M M");
+					
+					loose = true;
+					
+				}
+
+			}				
+		
+		}
+		
+		return(loose);
+		
+	}
+	
+}
---
+++ FishAreFood/src/game/PrintGameScreen.java
@@ -0,0 +1,55 @@
+package game;
+
+import java.util.Scanner;
+
+public class PrintGameScreen
+{
+
+	public static String printGameScreen(String[][] gameScreen, int score)
+	{
+		
+		//variables
+		
+		int gameScreenRows = 12;
+		int gameScreenCols = 24;
+		int rows = gameScreenRows;
+		int cols = gameScreenCols;
+		String sharkShift = "";
+		Scanner strInput = new Scanner(System.in);
+		
+		//printScore
+		
+		System.out.println("\nFishRFood                            Score = " + score);
+		
+		//printScreen
+		
+		for (int r = 0; r < rows; r++)
+		{
+			
+			for (int c = 0; c < cols; c++)
+			{
+			
+				System.out.print(gameScreen [r][c]);
+				
+			}
+			
+			if (r < rows - 1)
+			{
+				
+				System.out.print("\n");
+			
+			}
+			
+		}
+		
+		//getSharkShift
+	
+		//sharkShift = strInput.nextLine();
+		
+		//returnSharkShift
+		
+		return(sharkShift);
+		
+	}
+	
+}
